cc2639d9e1accd19f0d16793ede967b0948e4c28
ArcKos00 <105163313+ArcKos00@users.noreply.github.com>
2022-10-15 11:16:11 +0300

    AllDone

--- Delegates/Delegates/Class1.cs
+++ Delegates/Delegates/Class1.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Delegates
 {
-    internal class Class1
+    public class Class1
     {
+        private Action<bool> _action = Program.Show;
+        private Func<int, int, int> _toDo = Multiply;
+
+        public static int Multiply(int x, int y) => x * y;
+
+        public void ARARAT()
+        {
+            _action(new Class2().Calc(_toDo, ReturnValue(), ReturnValue())(ReturnValue()));
+        }
+
+        public int ReturnValue()
+        {
+            int x = 0;
+            Console.WriteLine("Введите число: ");
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out x))
+                {
+                    break;
+                }
+
+                Console.WriteLine("Введите корректное значение: ");
+            }
+
+            return x;
+        }
     }
 }
--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Delegates
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                new Class1().ARARAT();
+            }
+        }
+
+        public static void Show(bool success)
+        {
+            Console.WriteLine(success);
+        }
+    }
+}
